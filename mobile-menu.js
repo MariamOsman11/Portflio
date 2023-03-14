7b053fb682b4ccfdefd66f50f3532d86efc9deec
MariamOsman11 <mariamkca11@gmail.com>
2023-03-14 14:09:50 +0300

    Changes in mobile-menu

---
+++ mobile-menu.js
@@ -0,0 +1,14 @@
+const mobileMenu = document.querySelector('#mobile-menuid');
+const hamburgermenu = document.querySelector('#hamburger-menu-id');
+const closingmenu = document.querySelector('#closingmenu');
+
+
+function openmenu() {
+   document.getElementById("mobile-menu").style.visibility = "visible"
+}
+hamburgermenu.addEventListener('click', openmenu);
+closingmenu.addEventListener('click', closemenu);
+
+function closemenu() {
+   document.getElementById("mobile-menu").style.visibility = "hidden"
+}
