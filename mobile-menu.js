37def80c776f52413c6a323f03b080c3011a1b41
MariamOsman11 <mariamkca11@gmail.com>
2023-03-14 15:41:14 +0300

    Add changes in index,style and js

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,11 +1,9 @@
-// const mobileMenu = document.querySelector('#mobile-menuid');
 const hamburgermenu = document.querySelector('#hamburger-menu-id');
 const closingmenu = document.querySelector('#closingmenu');
 
 function openmenu() {
    document.getElementById('mobile-menu').style.visibility = 'visible';
 }
-
 hamburgermenu.addEventListener('click', openmenu);
 closingmenu.addEventListener('click', closemenu);
 
