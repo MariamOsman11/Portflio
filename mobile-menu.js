50f75fbde015c218777febca19285baf0ec916fc
MariamOsman11 <mariamkca11@gmail.com>
2023-03-14 20:52:48 +0300

    Change style and Js

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,10 +4,8 @@ const closingmenu = document.querySelector('#closingmenu');
 function openmenu() {
   document.getElementById('mobile-menu').style.visibility = 'visible';
   document.getElementById('main-header').style.filter = 'blur(8px)';
-
 }
 hamburgermenu.addEventListener('click', openmenu);
-
 function closemenu() {
   document.getElementById('mobile-menu').style.visibility = 'hidden';
   document.body.style.filter = 'blur(0)';
