f8b27084f3fd5ae8e9e203ef5ad1ef3078219ce2
MariamOsman11 <mariamkca11@gmail.com>
2023-03-14 20:44:11 +0300

    Change files

--- mobile-menu.js
+++ mobile-menu.js
@@ -3,10 +3,13 @@ const closingmenu = document.querySelector('#closingmenu');
 
 function openmenu() {
   document.getElementById('mobile-menu').style.visibility = 'visible';
+  document.getElementById('main-header').style.filter = 'blur(8px)';
+
 }
 hamburgermenu.addEventListener('click', openmenu);
 
 function closemenu() {
   document.getElementById('mobile-menu').style.visibility = 'hidden';
+  document.body.style.filter = 'blur(0)';
 }
 closingmenu.addEventListener('click', closemenu);
